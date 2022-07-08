afee63254b36eb4e5b0e77d21dab3f3e142402f8
surajthedeveloper <surajthedeveloper@gmail.com>
2022-07-08 14:28:09 +0530

    Deployment configuration for K8S

--- src/main/java/com/demo/kubernetes/k8sservice1/K8sService1Application.java
+++ src/main/java/com/demo/kubernetes/k8sservice1/K8sService1Application.java
@@ -1,5 +1,8 @@
 package com.demo.kubernetes.k8sservice1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class K8sService1Application {
-    @Value("${HOSTNAME:default}")
+    @Value("${HOSTNAME:service1}")
     private String hostname;
+
+    @Value("${MESSAGE:}")
+    private String message;
 
     public static void main(String[] args) {
         SpringApplication.run(K8sService1Application.class, args);
     }
 
-    @GetMapping("/hostname")
-    public String helloWorld() {
-        return hostname;
+    @GetMapping("/config")
+    public Config helloWorld() {
+        return new Config(hostname,message);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Config {
+        private String hostname;
+        private String message;
     }
 }
